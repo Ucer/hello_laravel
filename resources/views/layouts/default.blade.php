fa9a8a4210852a2a88b55b1a1a8394d7b1014288
zhjaaa <185429135@qq.com>
2017-05-06 16:37:54 +0800

    finish layouts and routes

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','Sina') - Laravel 优雅的开发</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    @include('layouts._header')
+  <div class="container">
+    @yield('content')
+    @include('layouts._footer')
+    </div>
+</body>
+</html>
