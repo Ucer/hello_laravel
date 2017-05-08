88855dd1963df57ab53105a92aa661c50269b100
zhjaaa <185429135@qq.com>
2017-05-09 07:29:16 +0800

    Add user status

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Status;
 use App\Models\User;
 use App\Policies\UserPolicy;
+use App\Policies\StatusPolicy;
 
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         User::class => UserPolicy::class,
+        Status::class => StatusPolicy::class,
     ];
 
     /**
